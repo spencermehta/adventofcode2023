58030ab70eee341dbed7c872a12ea6444a269c99
Spencer Mehta <spencermehta@gmail.com>
2023-12-19 20:59:54 +0000

    feat: day16 pt1 partial 2

--- java/my-app/src/main/java/com/mycompany/app/day16/Day16.java
+++ java/my-app/src/main/java/com/mycompany/app/day16/Day16.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.mycompany.app.utils.InputReader;
 
 public class Day16 {
+	private static int count = 0;
 	public static void main(String[] args) {
 		new Day16().solveA();
 	}
@@ -13,14 +14,11 @@ public class Day16 {
 		char[][] grid = readInput();
 		System.out.println(printGrid(grid));
 
-		int[][] energised = getEnergised(grid);
 		int[][][] visited = getVisited(grid);
-		int steps = step(grid, energised, visited, 0, 0, 'r');
-		System.out.println("steps " + steps);
+		step(grid, visited, 0, 0, 'r');
 
-		System.out.println(printIntGrid(energised));
 		System.out.println(printDistinctDirections(visited));
-		System.out.println(countEnergised(energised));
+		System.out.println(countEnergised(visited));
 	}
 
 	int dirToInt(char dir) {
@@ -35,92 +33,130 @@ public class Day16 {
 		}
 	}
 
-	int step(char[][] grid, int[][] energised, int[][][] visited, int y, int x, char dir) {
+	void step(char[][] grid, int[][][] visited, int y, int x, char dir) {
+		/*
+		if (count > 100) {
+			return;
+		}
+		*/
+		//System.out.println(printDistinctDirections(visited));
+		count++;
+		if ((y >= grid.length) || (y < 0) || (x >= grid[0].length) || (x < 0)) {
+			System.out.println("out of bounds");
+			return;
+		}
 		if (visited[y][x][dirToInt(dir)] > 0) {
-			return 0;
+			return;
 		}
 		visited[y][x][dirToInt(dir)] = 1;
 
-		System.out.println(String.format("Stepping %s %s %s", y, x, dir));
-		energised[y][x] += 1;
-		int nextY;
-		int nextX;
-		if (dir == 'r') {
-			nextY = y;
-			nextX = x+1;
-		} else if (dir == 'l') {
-			nextY = y;
-			nextX = x-1;
-		} else if (dir == 'u') {
-			nextY = y-1;
-			nextX = x;
-		} else {
-			nextY = y+1;
-			nextX = x;
-		}
+		System.out.println(String.format("Stepped on %s %s going %s", y, x, dir));
+		char c = grid[y][x];
 
-		if ((nextY >= grid.length) || (nextY < 0) || (nextX >= grid[0].length) || (nextX < 0)) {
-			System.out.println("out of bounds");
-			return 0;
-		}
-
-		char next = grid[nextY][nextX];
-		if (next == '.') {
-			return 1 + step(grid, energised, visited, nextY, nextX, dir);
-		} else if (next == '/') {
-			char newDir;
+		if (c == '.') {
+			int nextY;
+			int nextX;
 			if (dir == 'r') {
-				newDir = 'u';
+				nextY = y;
+				nextX = x+1;
 			} else if (dir == 'l') {
-				newDir = 'd';
+				nextY = y;
+				nextX = x-1;
 			} else if (dir == 'u') {
-				newDir = 'r';
+				nextY = y-1;
+				nextX = x;
 			} else {
-				newDir = 'l';
+				nextY = y+1;
+				nextX = x;
 			}
 
-			return 1 + step(grid, energised, visited, nextY, nextX, newDir);
-		} else if (next == '\\') {
+			step(grid, visited, nextY, nextX, dir);
+			return;
+		} else if (c == '/') {
 			char newDir;
+			int nextY;
+			int nextX;
 			if (dir == 'r') {
-				newDir = 'd';
-			} else if (dir == 'l') {
 				newDir = 'u';
+				nextY = y-1;
+				nextX = x;
+			} else if (dir == 'l') {
+				newDir = 'd';
+				nextY = y+1;
+				nextX = x;
 			} else if (dir == 'u') {
-				newDir = 'l';
-			} else {
 				newDir = 'r';
+				nextY = y;
+				nextX = x+1;
+			} else {
+				newDir = 'l';
+				nextY = y;
+				nextX = x+1;
 			}
 
-			return 1 + step(grid, energised, visited, nextY, nextX, newDir);
-		} else if (next == '-') {
+			step(grid, visited, nextY, nextX, newDir);
+			return;
+		} else if (c == '\\') {
+			char newDir;
+			int nextY;
+			int nextX;
+			if (dir == 'r') {
+				newDir = 'd';
+				nextY = y+1;
+				nextX = x;
+			} else if (dir == 'l') {
+				newDir = 'u';
+				nextY = y-1;
+				nextX = x;
+			} else if (dir == 'u') {
+				newDir = 'l';
+				nextY = y;
+				nextX = x-1;
+			} else {
+				newDir = 'r';
+				nextY = y;
+				nextX = x+1;
+			}
+
+			step(grid, visited, nextY, nextX, newDir);
+			return;
+		} else if (c == '-') {
 			if ((dir == 'r') || (dir == 'l')) {
-				return 1 + step(grid, energised, visited, nextY, nextX, dir);
+				int nextY = y;
+				int nextX;
+				if (dir == 'r') {
+					nextX = x+1;
+				} else {
+					nextX = x-1;
+				}
+				step(grid, visited, nextY, nextX, dir);
+				return;
 			} else {
 				System.out.println("splitting on -");
-				return 1 + step(grid, energised, visited, nextY, nextX, 'l') + 
-					step(grid, energised, visited, nextY, nextX, 'r');
+				step(grid, visited, y, x-1, 'l'); 
+				step(grid, visited, y, x+1, 'r');
+				return;
 			}
 		} else {
 			if ((dir == 'u') || (dir == 'd')) {
-				return 1 + step(grid, energised, visited, nextY, nextX, dir);
+				int nextY;
+				int nextX = x;
+				if (dir == 'u') {
+					nextY = y-1;
+				} else {
+					nextY = y+1;
+				}
+				step(grid, visited, nextY, nextX, dir);
+				return;
 			} else {
 				System.out.println("splitting on |");
-				return 1 + step(grid, energised, visited, nextY, nextX, 'u') + 
-					step(grid, energised, visited, nextY, nextX, 'd');
+				step(grid, visited, y-1, x, 'u');
+				step(grid, visited, y+1, x, 'd');
+				return;
 			}
 
 		}
-	}
 
-	int[][] getEnergised(char[][] grid) {
-		int[][] energised = new int[grid.length][grid[0].length];
-		for (int y = 0; y < grid.length; y++) {
-			for (int x = 0; x < grid[0].length; x++) {
-				energised[y][x] = 0;
-			}
-		}
-		return energised;
 	}
 
 	int[][][] getVisited(char[][] grid) {
@@ -134,21 +170,6 @@ public class Day16 {
 			}
 		}
 		return visited;
-	}
-
-	int[][][] copy(int[][][] grid) {
-		int[][][] newGrid = new int[grid.length][grid[0].length][grid[0][0].length];
-
-		for (int y = 0; y < grid.length; y++) {
-			for (int x = 0; x < grid[0].length; x++) {
-				for (int j = 0; j < grid[0][0].length; j++) {
-					newGrid[y][x][j] = grid[y][x][j];
-				}
-
-			}
-		}
-
-		return newGrid;
 	}
 
 	String printGrid(char[][] grid) {
@@ -173,11 +194,17 @@ public class Day16 {
 		return s;
 	}
 
-	int countEnergised(int[][] grid) {
+	int countEnergised(int[][][] grid) {
 		int sum = 0;
-		for (int[] line : grid) {
-			for (int c : line) {
-				if (c != 0) {
+		for (int[][] line : grid) {
+			for (int[] c : line) {
+				int s = 0;
+				for (int i : c) {
+					if (i > 0) {
+						s += 1;
+					}
+				}
+				if (s > 0) {
 					sum += 1;
 				}
 			}
@@ -195,7 +222,11 @@ public class Day16 {
 						sum += 1;
 					}
 				}
-				s += String.valueOf(sum);
+				if (sum > 0) {
+					s += "#";
+				} else {
+					s += ".";
+				}
 			}
 			s += "\n";
 		}
