ce55908e9d069d2f39b57439f55f15cc0931e80f
Spencer Mehta <spencermehta@gmail.com>
2023-12-19 18:29:03 +0000

    feat: day15 pt2

--- java/my-app/src/main/java/com/mycompany/app/day15/Day15.java
+++ java/my-app/src/main/java/com/mycompany/app/day15/Day15.java
@@ -1,13 +1,18 @@
 package com.mycompany.app.day15;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import com.mycompany.app.utils.InputReader;
 
 public class Day15 {
 	public static void main(String[] args) {
-		new Day15().solveA();
+		new Day15().solveB();
 	}
 
 	public void solveA() {
@@ -19,6 +24,63 @@ public class Day15 {
 			sum += hash(line);
 		}
 
+		System.out.println(sum);
+	}
+
+	public void solveB() {
+		Map<Integer, Map<String, String>> map = new HashMap<>();
+		List<String> lines = readInput();
+
+		for (String line : lines) {
+			if (line.contains("=")) {
+				System.out.println("eq " + line);
+				String key = line.split("=")[0];
+				String val = line.split("=")[1];
+
+				int hash = hash(key);
+				Map<String, String> bucket = map.getOrDefault(hash, new LinkedHashMap<String, String>());
+
+				/*
+				if (bucket.containsKey(val)) {
+					bucket.remove(key);
+				}
+				*/
+				bucket.put(key, val);
+				map.put(hash, bucket);
+
+				
+
+
+			} else {
+				System.out.println("del " + line);
+				String key = line.split("-")[0];
+				int hash = hash(key);
+
+				Map<String, String> bucket = map.getOrDefault(hash, new LinkedHashMap<String, String>());
+				bucket.remove(key);
+				map.put(hash, bucket);
+			}
+		}
+
+		System.out.println(map);
+		int sum = 0;
+
+		for (Entry<Integer, Map<String, String>> entry : map.entrySet()) {
+			int boxN = entry.getKey() + 1;
+
+			int i = 1;
+			for (Entry<String, String> subEntry : entry.getValue().entrySet()) {
+				int slot = i;
+				int focalLength = Integer.parseInt(subEntry.getValue());
+				int product = boxN * slot * focalLength;
+				System.out.println("product of " + subEntry.getKey() + " " + product);
+				sum += product;
+
+				i++;
+					
+			}
+
+		}
 		System.out.println(sum);
 	}
 
